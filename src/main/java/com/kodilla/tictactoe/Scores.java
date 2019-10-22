c6297433d534af161b32cfc5ca0a221ad3021e5a
deseff <blazej.kurjata@gmail.com>
2019-10-22 04:03:49 +0200

    TicTacToe next steps: - WinnerFinder class removed - clean up the code - improvement of Tic Tac Toe mechanics

--- src/main/java/com/kodilla/tictactoe/Scores.java
+++ src/main/java/com/kodilla/tictactoe/Scores.java
@@ -6,11 +6,61 @@ public class Scores {
 
     private int playerSavedScore;
     private int computerSavedScore;
-
-    private int[] saveScores = {playerSavedScore, computerSavedScore};
+    private TicTacToeController ticTacToeController;
 
     private ClassLoader classLoader = getClass().getClassLoader();
     private File scores = new File(classLoader.getResource("files/Scores.txt").getFile());
+
+    public Scores(TicTacToeController ticTacToeController) {
+        this.ticTacToeController = ticTacToeController;
+
+        readScore();
+    }
+
+    public void setPlayerWon(){
+        playerSavedScore++;
+        writeFile();
+    }
+
+    public void setComputerWon(){
+        computerSavedScore++;
+        writeFile();
+    }
+
+    public void writeFile(){
+        writeFile(playerSavedScore+"-"+computerSavedScore);
+    }
+
+    public void writeFile(String value) {
+        try {
+            BufferedWriter outputWriter = new BufferedWriter(new FileWriter(scores));
+            outputWriter.write(value);
+            outputWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readScore() {
+        String fileText = "0-0";
+        try {
+            BufferedReader inputReader = new BufferedReader(new FileReader(scores));
+
+            fileText = inputReader.readLine();
+            System.out.println(fileText);
+            inputReader.close();
+        } catch (Exception e) {
+            //create new file with 0-0 score
+            writeFile("0-0");
+        }
+
+        String parts[] = fileText.split("-");
+        String playerScoreString = parts[0];
+        String computerScoreString = parts[1];
+
+        this.playerSavedScore = Integer.valueOf(playerScoreString);
+        this.computerSavedScore = Integer.valueOf(computerScoreString);
+    }
 
     public int getPlayerSavedScore() {
         return playerSavedScore;
@@ -20,30 +70,10 @@ public class Scores {
         return computerSavedScore;
     }
 
-    TicTacToeController controller = new TicTacToeController();
-
-    public void writeFile() {
-        try {
-            BufferedWriter outputWriter = new BufferedWriter(new FileWriter(scores));
-            outputWriter.write(controller.getScoresToWrite());
-            outputWriter.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void readFile() {
-        try {
-            BufferedReader inputReader = new BufferedReader(new FileReader(scores));
-
-            playerSavedScore = saveScores[0];
-            computerSavedScore = saveScores[1];
-
-            String fileText = inputReader.readLine();
-            System.out.println(fileText);
-            inputReader.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @Override
+    public String toString() {
+        return "Scores: " +
+                "Player " + playerSavedScore + "-" +
+                computerSavedScore + " Computer";
     }
 }
--- src/main/java/com/kodilla/tictactoe/TicTacToeController.java
+++ src/main/java/com/kodilla/tictactoe/TicTacToeController.java
@@ -21,10 +21,128 @@ public class TicTacToeController {
     private boolean playerMoved;
     private int computerScore;
     private int playerScore;
-    private String scoresToWrite;
 
     private Square playersSquare;
     private Square computersSquare;
+
+    private Scores scores = new Scores(this);
+
+    public void handleOnMouseClicked(Square square) {
+        int col = square.getCol();
+        int row = square.getRow();
+
+        System.out.println("Clicked col " + col + ", row " + row);
+
+        Random random = new Random();
+        scores.readScore();
+        playerScore = scores.getPlayerSavedScore();
+        computerScore = scores.getComputerSavedScore();
+
+        playersSquare = square;
+
+        //PLAYER MOVE
+        playerMoved = false;
+        if (!hasPlayerWon && !hasComputerWon) {
+            if (!square.getIsSquareUsed()) {
+                square.setFill(new ImagePattern(this.userShape));
+                square.setSquareUsed(true);
+                square.setShape(this.userShape.toString());
+                playerMoved = true;
+            }
+            //CHECK IF PLAYER WON
+            for (int c = 0; c < 3; c++) {
+                if (squares.get(c + "-0").equals(square) && squares.get(c + "-1").equals(square) && squares.get(c + "-2").equals(square)) {
+                    setHasPlayerWon(true);
+                    playerWon();
+                }
+            }
+            for (int r = 0; r < 3; r++) {
+                if (!hasPlayerWon && squares.get("0-" + r).equals(square) && squares.get("1-" + r).equals(square) && squares.get("2-" + r).equals(square)) {
+                    setHasPlayerWon(true);
+                    playerWon();
+                }
+            }
+            if (!hasPlayerWon && squares.get("0-0").equals(square) && squares.get("1-1").equals(square) && squares.get("2-2").equals(square)) {
+                setHasPlayerWon(true);
+                playerWon();
+            }
+            if (!hasPlayerWon && squares.get("2-0").equals(square) && squares.get("1-1").equals(square) && squares.get("0-2").equals(square)) {
+                setHasPlayerWon(true);
+                playerWon();
+            }
+        }
+
+        //COMPUTER MOVE
+
+        long freeSquares = squares.values().stream()
+                .filter(s -> !s.getIsSquareUsed())
+                .count();
+        System.out.println("free squares " + freeSquares);
+
+        if (!hasComputerWon && !hasPlayerWon && playerMoved && freeSquares > 0) {
+            do {
+                col = random.nextInt(3);
+                row = random.nextInt(3);
+                Square computerChoice = squares.get(col + "-" + row);
+                computersSquare = computerChoice;
+                System.out.println("Computer choice " + col + "-" + row);
+
+                if (!computerChoice.getIsSquareUsed()) {
+                    computerChoice.setFill(new ImagePattern(this.computerShape));
+                    computerChoice.setSquareUsed(true);
+                    computerChoice.setShape(this.computerShape.toString());
+                    computerMoved = true;
+                }
+
+                //CHECK IF COMPUTER WON
+                for (int c = 0; c < 3; c++) {
+                    if (squares.get(c + "-0").equals(computerChoice) && squares.get(c + "-1").equals(computerChoice) && squares.get(c + "-2").equals(computerChoice)) {
+                        setHasComputerWon(true);
+                        computerWon();
+                    }
+                }
+                for (int r = 0; r < 3; r++) {
+                    if (!hasComputerWon && squares.get("0-" + r).equals(computerChoice) && squares.get("1-" + r).equals(computerChoice) && squares.get("2-" + r).equals(computerChoice)) {
+                        setHasComputerWon(true);
+                        computerWon();
+                    }
+                }
+                if (!hasComputerWon && squares.get("0-0").equals(computerChoice) && squares.get("1-1").equals(computerChoice) && squares.get("2-2").equals(computerChoice)) {
+                    setHasComputerWon(true);
+                    computerWon();
+                }
+                if (!hasComputerWon && squares.get("2-0").equals(computerChoice) && squares.get("1-1").equals(computerChoice) && squares.get("0-2").equals(computerChoice)) {
+                    setHasComputerWon(true);
+                    computerWon();
+                }
+            } while (!computerMoved);
+        }
+        computerMoved = false;
+        if (hasComputerWon || hasPlayerWon) {
+            System.out.println("Player's score equals " + playerScore);
+            System.out.println("Computer's score equals " + computerScore);
+        } else if (!hasPlayerWon && !hasComputerWon && freeSquares == 0) {
+            draw();
+        }
+        scores.writeFile();
+    }
+
+    private void playerWon() {
+        System.out.println("You won!");
+        BoxesCreator.showEndBox("You won!");
+        scores.setPlayerWon();
+    }
+
+    private void computerWon() {
+        System.out.println("Computer won!");
+        BoxesCreator.showEndBox("Computer won!");
+        scores.setComputerWon();
+    }
+
+    private void draw() {
+        System.out.println("Draw");
+        BoxesCreator.showEndBox("Draw");
+    }
 
     public Image getCross() {
         return cross;
@@ -54,161 +172,8 @@ public class TicTacToeController {
         this.hasComputerWon = hasComputerWon;
     }
 
-    public boolean isHasPlayerWon() {
-        return hasPlayerWon;
-    }
-
-    public boolean isHasComputerWon() {
-        return hasComputerWon;
-    }
-
-    public boolean isComputerMoved() {
-        return computerMoved;
-    }
-
-    public boolean isPlayerMoved() {
-        return playerMoved;
-    }
-
-    public String getScoresToWrite() {
-        return scoresToWrite;
-    }
-
-    public Square getPlayersSquare() {
-        return playersSquare;
-    }
-
-    public Square getComputersSquare() {
-        return computersSquare;
-    }
-
-    public int getComputerScore() {
-        return computerScore;
-    }
-
-    public int getPlayerScore() {
-        return playerScore;
-    }
-
-    public Map<String, Square> getSquares() {
-        return squares;
-    }
-
-    Scores scores = new Scores();
-
-
-    public void handleOnMouseClicked(Square square) {
-        int col = square.getCol();
-        int row = square.getRow();
-
-        System.out.println("Clicked col " + col + ", row " + row);
-
-        Random random = new Random();
-        scores.readFile();
-        playerScore = scores.getPlayerSavedScore();
-        computerScore = scores.getComputerSavedScore();
-
-        playersSquare = square;
-
-        //PLAYER MOVE
-        playerMoved = false;
-        if (!hasPlayerWon && !hasComputerWon) {
-            if (!square.getIsSquareUsed()) {
-                square.setFill(new ImagePattern(this.userShape));
-//                square.setFill(new ImagePattern(cross));
-                square.setSquareUsed(true);
-                square.setShape(this.userShape.toString());
-                playerMoved = true;
-//                square.setShape(cross.toString());
-            }
-            //CHECK IF PLAYER WON
-            for (int c = 0; c < 3; c++) {
-                if (squares.get(c + "-0").equals(square) && squares.get(c + "-1").equals(square) && squares.get(c + "-2").equals(square)) {
-                    setHasPlayerWon(true);
-                    playerScore += 1;
-                    System.out.println("You won!");
-                }
-            }
-            for (int r = 0; r < 3; r++) {
-                if (!hasPlayerWon && squares.get("0-" + r).equals(square) && squares.get("1-" + r).equals(square) && squares.get("2-" + r).equals(square)) {
-                    setHasPlayerWon(true);
-                    playerScore += 1;
-                    System.out.println("You won!");
-                }
-            }
-            if (!hasPlayerWon && squares.get("0-0").equals(square) && squares.get("1-1").equals(square) && squares.get("2-2").equals(square)) {
-                setHasPlayerWon(true);
-                playerScore += 1;
-                System.out.println("You won!");
-            }
-            if (!hasPlayerWon && squares.get("2-0").equals(square) && squares.get("1-1").equals(square) && squares.get("0-2").equals(square)) {
-                setHasPlayerWon(true);
-                playerScore += 1;
-                System.out.println("You won!");
-            }
-        }
-
-        //COMPUTER MOVE
-
-        /*Optional<Square> firstFreeSquareOpt = squares.values().stream()
-                .filter(s -> !s.getIsSquareUsed())
-                .findFirst();
-
-        if (!firstFreeSquareOpt.isPresent()) {
-            System.out.println("DRAW");
-        }
-        Square computerChoice = firstFreeSquareOpt.get();*/
-        computerMoved = false;
-        if (!hasComputerWon && !hasPlayerWon && playerMoved) {
-            do {
-                col = random.nextInt(3);
-                row = random.nextInt(3);
-                Square computerChoice = squares.get(col + "-" + row);
-                computersSquare = computerChoice;
-                System.out.println("Computer choice " + col + "-" + row);
-
-                if (!computerChoice.getIsSquareUsed()) {
-                    computerChoice.setFill(new ImagePattern(this.computerShape));
-//            computerChoice.setFill(new ImagePattern(nought));
-                    computerChoice.setSquareUsed(true);
-                    computerChoice.setShape(this.computerShape.toString());
-                    computerMoved = true;
-//            computerChoice.setShape(nought.toString());
-                }
-
-                //CHECK IF COMPUTER WON
-                for (int c = 0; c < 3; c++) {
-                    if (squares.get(c + "-0").equals(computerChoice) && squares.get(c + "-1").equals(computerChoice) && squares.get(c + "-2").equals(computerChoice)) {
-                        setHasComputerWon(true);
-                        computerScore += 1;
-                        System.out.println("Computer won!");
-                    }
-                }
-                for (int r = 0; r < 3; r++) {
-                    if (!hasComputerWon && squares.get("0-" + r).equals(computerChoice) && squares.get("1-" + r).equals(computerChoice) && squares.get("2-" + r).equals(computerChoice)) {
-                        setHasComputerWon(true);
-                        computerScore += 1;
-                        System.out.println("Computer won!");
-                    }
-                }
-                if (!hasComputerWon && squares.get("0-0").equals(computerChoice) && squares.get("1-1").equals(computerChoice) && squares.get("2-2").equals(computerChoice)) {
-                    setHasComputerWon(true);
-                    computerScore += 1;
-                    System.out.println("Computer won!");
-                }
-                if (!hasComputerWon && squares.get("2-0").equals(computerChoice) && squares.get("1-1").equals(computerChoice) && squares.get("0-2").equals(computerChoice)) {
-                    setHasComputerWon(true);
-                    computerScore += 1;
-                    System.out.println("Computer won!");
-                }
-            } while (!computerMoved);
-        }
-        if (hasComputerWon || hasPlayerWon) {
-            System.out.println("Player's score equals " + playerScore);
-            System.out.println("Computer's score equals " + computerScore);
-            scoresToWrite = playerScore + "\n" + computerScore;
-        }
-        scores.writeFile();
+    public Scores getScores() {
+        return scores;
     }
 }
 
